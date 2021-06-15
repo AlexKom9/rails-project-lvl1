196977b93fb70cac9d23e4fc94156c66ad8ec6fd
Alex Komar <909komar@gmail.com>
2021-06-15 11:54:05 +0300

    setup rubocop

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,24 +6,24 @@ module HexletCode
   class Error < StandardError; end
   # Your code goes here...
 
+  # HTML tags builder module
   module Tag
     class Error < StandardError; end
-  
+
     class << self
-      def build type, params = {}, &block
+      def build(type, params = {})
         if block_given?
-          "<#{type} #{prams_to_attr params}>#{ yield }</#{type}>"
+          "<#{type}#{prams_to_attr params}>#{yield}</#{type}>"
         else
-          "<#{type} #{prams_to_attr params}/>"
+          "<#{type}#{prams_to_attr params}/>"
         end
       end
-  
+
       private
-  
-      def prams_to_attr params
-        params.reduce("") { |accum, (key, value)| accum += " #{key}='#{value}'" }
+
+      def prams_to_attr(params)
+        params.reduce("") { |accum, (key, value)| accum + " #{key}='#{value}'" }
       end
     end
   end
-  
 end
