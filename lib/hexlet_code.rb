712f180f041221163d72ef271f9fc38579fd1219
Alex Komar <909komar@gmail.com>
2021-06-22 12:48:06 +0300

    implement input method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -13,7 +13,7 @@ module HexletCode
     class << self
       def build(type, params = {})
         if block_given?
-          "<#{type}#{prams_to_attr params}>#{yield}</#{type}>"
+          "<#{type}#{prams_to_attr params}>\n#{yield}\n</#{type}>"
         else
           "<#{type}#{prams_to_attr params}/>"
         end
@@ -25,11 +25,47 @@ module HexletCode
         params.reduce("") { |accum, (key, value)| accum + " #{key}='#{value}'" }
       end
     end
+
+    module PublicTags
+      class << self 
+        @@temp_tegs_result = ''
+
+        def input name = 'name', as: :input, **args
+          # require 'byebug'; byebug;
+          tag_name = as == :text ? 'textarea' : 'input'
+
+          @@temp_tegs_result += Tag.build tag_name, { name: name, **args } do 
+            yield if block_given? 
+          end
+        end
+
+        # TODO: make it private
+        def reset
+          temp_tegs_result = ''
+        end
+      end
+    end
   end
 
   class << self
-    def form_for(_entity = {}, &block)
-      Tag.build("form", { action: "#", method: "post" }, &block)
+    def form_for(_entity = {})
+      result = Tag.build "form", { action: "#", method: "post" } do
+        result = yield Tag::PublicTags if block_given?
+
+        Tag::PublicTags.reset()
+        result 
+      end
     end
   end
 end
+
+
+# User = Struct.new(:name, :job, keyword_init: true)
+# user = User.new name: 'rob', job: 'hexlet'
+
+# result = HexletCode.form_for user do |f|
+#   f.input :name
+#   f.input :job, as: :text
+# end
+
+# p result
