84289d64930997d8095882eac7744af4b0adffaa
Alex Komar <909komar@gmail.com>
2021-06-29 10:36:04 +0300

    refactoring

---
+++ lib/form_builder.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+module HexletCode
+  class FormBuilder
+    def initialize(entity)
+      @entity = entity
+      @temp_tegs_result = ''
+    end
+
+    def input(name, **options)
+      id = options.fetch(:id, name)
+      as = options.fetch(:as, :input)
+
+      prepared_options = options.clone
+      prepared_options.delete(:id)
+      prepared_options.delete(:as)
+
+      case as
+      when :input
+        build_input name: name, id: id, **prepared_options
+      when :text
+        build_textarea name: name, id: id, **prepared_options
+      when :checkbox
+        build_checkbox name: name, id: id, **prepared_options
+      when :select
+        build_select name: name, id: id, **prepared_options
+      else
+        raise 'Invalid options.as'
+      end
+    end
+
+    def submit(text = 'Save', **options)
+      add_tag do
+        Tag.build 'input', { type: 'submit', **options, value: text, name: 'commit' }
+      end
+    end
+
+    private
+
+    def add_new_line
+      @temp_tegs_result += "\n"
+    end
+
+    def add_tag
+      @temp_tegs_result += yield
+
+      add_new_line
+    end
+
+    def build_label(title, html_for, **options)
+      add_tag do
+        Tag.build('label', { for: html_for, **options }) { title }
+      end
+    end
+
+    def build_input(name:, id:, **options)
+      add_tag do
+        Tag.build('label', { for: id }) { name }
+      end
+
+      add_tag do
+        Tag.build('input', { name: name, value: @entity[name], id: id, type: :text, **options })
+      end
+    end
+
+    def build_textarea(name:, id:, **options)
+      add_tag do
+        Tag.build('label', { for: id }) { name }
+      end
+
+      add_tag do
+        Tag.build('textarea', { name: name, id: id, **options }) { @entity[name] }
+      end
+    end
+
+    def build_select(name:, id:, **options)
+      add_tag do
+        Tag.build('select', { name: name, value: @entity[name], id: id }) do
+          add_new_line
+          result = "\n"
+          options[:options].each do |option_value|
+            result += Tag.build('option', { value: option_value }) do
+              option_value
+            end
+            result += "\n"
+          end
+          add_new_line
+          result
+        end
+      end
+    end
+
+    def build_checkbox(name:, id:, **options)
+      add_tag do
+        Tag.build('input', { name: name, checked: @entity[name], value: name, id: id, **options, type: :checkbox })
+      end
+      add_tag do
+        Tag.build('label', { for: id }) { name }
+      end
+    end
+  end
+end
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-module Tag
-  class << self
-    def build(type, params = {})
-      if block_given?
-        "<#{type}#{prams_to_attr params}>#{yield}</#{type}>"
-      else
-        "<#{type}#{prams_to_attr params}/>"
+module HexletCode
+  class Tag
+    class << self
+      def build(type, params = {})
+        if block_given?
+          "<#{type}#{prams_to_attr params}>#{yield}</#{type}>"
+        else
+          "<#{type}#{prams_to_attr params}/>"
+        end
       end
-    end
 
-    private
+      private
 
-    def prams_to_attr(params)
-      params.reduce('') { |accum, (key, value)| accum + " #{key}='#{value}'" }
+      def prams_to_attr(params)
+        params.reduce('') { |accum, (key, value)| accum + " #{key}='#{value}'" }
+      end
     end
   end
 end
